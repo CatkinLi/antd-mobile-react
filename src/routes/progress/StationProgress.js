2b8e9400eac587798cbf3b0a31fbff45cd90c44f
lipengfei <123456>
2020-01-08 15:30:18 +0800

    添加bizcharts 插件 项目展届信息列表

--- src/routes/progress/StationProgress.js
+++ src/routes/progress/StationProgress.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import { connect } from 'dva';
 import { Grid, WhiteSpace, List } from 'antd-mobile';
+import { Chart, Tooltip, Geom, Guide } from 'bizcharts';
 import styles from './StationProgress.css';
 
-const data = [
+const { Text } = Guide;
+const data = [{
+  gender: '索票',
+  value: 20,
+}, {
+  gender: '有效',
+  value: 50,
+}, {
+  gender: '到场',
+  value: 70,
+}, {
+  gender: '订单',
+  value: 92.1,
+}];
+
+const scale = {
+  value: {
+    min: 0,
+    max: 100,
+  },
+};
+
+const data1 = [
   {
     title: '订单总进度',
     value: '80.67%',
@@ -37,7 +60,7 @@ class StationProgress extends React.Component{
             </div>
             <div className={ styles.progress_count}>
               <Grid
-                data={data}
+                data={data1}
                 columnNum={2}
                 itemStyle={{ height: 99 }}
                 renderItem={item => (
@@ -52,14 +75,354 @@ class StationProgress extends React.Component{
               />
             </div>
             <WhiteSpace size="sm"/>
-            <div style={{ margin: 10, backgroundColor: '#d8f1ed' }}>
-              <div>
+            <div style={{ margin: 10, height: 190, backgroundColor: '#d8f1ed' }}>
+              <div style={{ padding: 10 }}>
                 <div>
-                  上海 8.9-8.11<span style={{ marginLeft: 20, width: 60, backgroundColor: '#92c152'}}> 已开展</span>
-                  <span style={{ float: 'right' }}>></span>
+                  <span style={{ fontSize: 16, fontWeight: 550 }}>上海 8.9-8.11</span>
+                  <span className={styles.progress_on_time}> 已开展</span>
+                  <span style={{ float: 'right' }}></span>
                 </div>
-                <div style={{ height: 80 }}>
-                  索票
+                <div>
+                  <Chart
+                    height={100}
+                    padding={{ left: 0, top: 30, right: 0, bottom: 0 }}
+                    data={data}
+                    scale={scale}
+                    forceFit>
+                    <Tooltip/>
+                    <Geom
+                      type="interval"
+                      position="gender*value"
+                      color={['gender', ['#FDBDCA', '#F79FB1', '#F58095', '#F2637A']]}
+                      shape="liquid-fill-gauge"
+                      style={{
+                        lineWidth: 4,
+                        opacity: 0.75,
+                      }}
+                    />
+                    <Guide>
+                      {
+                        data.map(
+                          row => (<Text
+                            content={`${row.value.toFixed(0)}%`}
+                            top
+                            position={{
+                              gender: row.gender,
+                              value: 50,
+                            }}
+                            style={{
+                              opacity: 0.75,
+                              fontSize: window.innerWidth / 20,
+                              textAlign: 'center',
+                            }}
+                          />))
+                      }
+                    </Guide>
+                  </Chart>
+                </div>
+                <div style={{ height: 30, textAlign: 'center', lineHeight: '30px' }}>
+                  <div className={styles.progress_cell_text}>索票</div>
+                  <div className={styles.progress_cell_text}>有效</div>
+                  <div className={styles.progress_cell_text}>到场</div>
+                  <div className={styles.progress_cell_text}>订单</div>
+                </div>
+                <div style={{ height: 30, textAlign: 'center' }}>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <WhiteSpace size="sm"/>
+            <div style={{ margin: 10, height: 190, backgroundColor: '#d8f1ed' }}>
+              <div style={{ padding: 10 }}>
+                <div>
+                  <span style={{ fontSize: 16, fontWeight: 550 }}>上海 8.9-8.11</span>
+                  <span className={styles.progress_on_time}> 已开展</span>
+                  <span style={{ float: 'right' }}></span>
+                </div>
+                <div>
+                  <Chart
+                    height={100}
+                    padding={{ left: 0, top: 30, right: 0, bottom: 0 }}
+                    data={data}
+                    scale={scale}
+                    forceFit>
+                    <Tooltip/>
+                    <Geom
+                      type="interval"
+                      position="gender*value"
+                      color={['gender', ['#FDBDCA', '#F79FB1', '#F58095', '#F2637A']]}
+                      shape="liquid-fill-gauge"
+                      style={{
+                        lineWidth: 4,
+                        opacity: 0.75,
+                      }}
+                    />
+                    <Guide>
+                      {
+                        data.map(
+                          row => (<Text
+                            content={`${row.value}%`}
+                            top
+                            position={{
+                              gender: row.gender,
+                              value: 50,
+                            }}
+                            style={{
+                              opacity: 0.75,
+                              fontSize: window.innerWidth / 20,
+                              textAlign: 'center',
+                            }}
+                          />))
+                      }
+                    </Guide>
+                  </Chart>
+                </div>
+                <div style={{ height: 30, textAlign: 'center', lineHeight: '30px' }}>
+                  <div className={styles.progress_cell_text}>索票</div>
+                  <div className={styles.progress_cell_text}>有效</div>
+                  <div className={styles.progress_cell_text}>到场</div>
+                  <div className={styles.progress_cell_text}>订单</div>
+                </div>
+                <div style={{ height: 30, textAlign: 'center' }}>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/*<List>
+                <Item
+                  multipleLine
+                  onClick={() => {}}
+                  platform="android"
+                >
+
+                </Item>
+                <Item
+                  multipleLine
+                  onClick={() => {}}
+                  platform="android"
+                >
+                  <div>
+                    <div>
+                      上海 8.9-8.11<span style={{ marginLeft: 20, width: 60, backgroundColor: '#92c152'}}> 已开展</span>
+                      <span style={{ float: 'right' }}>></span>
+                    </div>
+                    <div>
+                      索票
+                    </div>
+                  </div>
+                </Item>
+              </List>*/}
+            </div>
+            <WhiteSpace size="sm"/>
+            <div style={{ margin: 10, height: 190, backgroundColor: '#d8f1ed' }}>
+              <div style={{ padding: 10 }}>
+                <div>
+                  <span style={{ fontSize: 16, fontWeight: 550 }}>上海 8.9-8.11</span>
+                  <span className={styles.progress_on_time}> 已开展</span>
+                  <span style={{ float: 'right' }}></span>
+                </div>
+                <div>
+                  <Chart
+                    height={100}
+                    padding={{ left: 0, top: 30, right: 0, bottom: 0 }}
+                    data={data}
+                    scale={scale}
+                    forceFit>
+                    <Tooltip/>
+                    <Geom
+                      type="interval"
+                      position="gender*value"
+                      color={['gender', ['#FDBDCA', '#F79FB1', '#F58095', '#F2637A']]}
+                      shape="liquid-fill-gauge"
+                      style={{
+                        lineWidth: 4,
+                        opacity: 0.75,
+                      }}
+                    />
+                    <Guide>
+                      {
+                        data.map(
+                          row => (<Text
+                            content={`${row.value}%`}
+                            top
+                            position={{
+                              gender: row.gender,
+                              value: 50,
+                            }}
+                            style={{
+                              opacity: 0.75,
+                              fontSize: window.innerWidth / 20,
+                              textAlign: 'center',
+                            }}
+                          />))
+                      }
+                    </Guide>
+                  </Chart>
+                </div>
+                <div style={{ height: 30, textAlign: 'center', lineHeight: '30px' }}>
+                  <div className={styles.progress_cell_text}>索票</div>
+                  <div className={styles.progress_cell_text}>有效</div>
+                  <div className={styles.progress_cell_text}>到场</div>
+                  <div className={styles.progress_cell_text}>订单</div>
+                </div>
+                <div style={{ height: 30, textAlign: 'center' }}>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/*<List>
+                <Item
+                  multipleLine
+                  onClick={() => {}}
+                  platform="android"
+                >
+
+                </Item>
+                <Item
+                  multipleLine
+                  onClick={() => {}}
+                  platform="android"
+                >
+                  <div>
+                    <div>
+                      上海 8.9-8.11<span style={{ marginLeft: 20, width: 60, backgroundColor: '#92c152'}}> 已开展</span>
+                      <span style={{ float: 'right' }}>></span>
+                    </div>
+                    <div>
+                      索票
+                    </div>
+                  </div>
+                </Item>
+              </List>*/}
+            </div>
+            <WhiteSpace size="sm"/>
+            <div style={{ margin: 10, height: 190, backgroundColor: '#d8f1ed' }}>
+              <div style={{ padding: 10 }}>
+                <div>
+                  <span style={{ fontSize: 16, fontWeight: 550 }}>上海 8.9-8.11</span>
+                  <span className={styles.progress_on_time}> 已开展</span>
+                  <span style={{ float: 'right' }}></span>
+                </div>
+                <div>
+                  <Chart
+                    height={100}
+                    padding={{ left: 0, top: 30, right: 0, bottom: 0 }}
+                    data={data}
+                    scale={scale}
+                    forceFit>
+                    <Tooltip/>
+                    <Geom
+                      type="interval"
+                      position="gender*value"
+                      color={['gender', ['#FDBDCA', '#F79FB1', '#F58095', '#F2637A']]}
+                      shape="liquid-fill-gauge"
+                      style={{
+                        lineWidth: 4,
+                        opacity: 0.75,
+                      }}
+                    />
+                    <Guide>
+                      {
+                        data.map(
+                          row => (<Text
+                            content={`${row.value}%`}
+                            top
+                            position={{
+                              gender: row.gender,
+                              value: 50,
+                            }}
+                            style={{
+                              opacity: 0.75,
+                              fontSize: window.innerWidth / 20,
+                              textAlign: 'center',
+                            }}
+                          />))
+                      }
+                    </Guide>
+                  </Chart>
+                </div>
+                <div style={{ height: 30, textAlign: 'center', lineHeight: '30px' }}>
+                  <div className={styles.progress_cell_text}>索票</div>
+                  <div className={styles.progress_cell_text}>有效</div>
+                  <div className={styles.progress_cell_text}>到场</div>
+                  <div className={styles.progress_cell_text}>订单</div>
+                </div>
+                <div style={{ height: 30, textAlign: 'center' }}>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
+                  <div className={styles.progress_cell_num}>
+                    <div>
+                      6000/5000
+                    </div>
+                  </div>
                 </div>
               </div>
               {/*<List>
